cafbe8b8dee57b79d049e39ed750553b1e723257
Andrey Pylnev <147094032+flexer2006@users.noreply.github.com>
2024-12-22 18:16:41 +0300

    Update applicationErrors.go

--- internal/application/applicationErrors.go
+++ internal/application/applicationErrors.go
@@ -3,14 +3,15 @@ package application
 import "errors"
 
 var (
-	// Пустой запрос
+	
 	ErrEmptyExpression = errors.New("expression is required")
-	// Неправильный JSON формат
+	
 	ErrInvalidJSON = errors.New("invalid JSON format")
 )
 
 type ErrorResponse struct {
-	Error       string `json:"error"`                 // Сообщение об ошибке
-	Code        int    `json:"code"`                  // Код ошибки
-	Description string `json:"description,omitempty"` // Описание ошибки (необязательное поле)
+	Error       string `json:"error"`    
+	Code        int    `json:"code"`  
+	Description string `json:"description,omitempty"` 
 }
+// заготовочка
